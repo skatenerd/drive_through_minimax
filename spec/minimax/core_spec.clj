5dc47dcddf2c9e3adff6eda812a5cb3fae7e467d
Wai Lee Chin Feman <wchinfeman@8thlight.com>
2013-03-04 18:41:36 -0600

    rename move-history to path-to-state, start using vector

--- spec/minimax/core_spec.clj
+++ spec/minimax/core_spec.clj
@@ -20,36 +20,40 @@
 
 (describe "Scoring"
   (it "scores current victory"
-    (let [win-state (new-test-state [{:round 0 :player 0 :move-history :0} []])
-          terminal-scorer (new-terminal-scorer {{:round 0 :player 0 :move-history :0} 1})]
+    (let [win-state
+          (new-test-state
+            [{:round 0 :player 0 :path-to-state :0} []])
+          terminal-scorer
+          (new-terminal-scorer {
+            {:round 0 :player 0 :path-to-state :0} 1
+          })]
       (should= 1 (score-for-current-player win-state terminal-scorer))))
 
   (it "looks one move ahead and sees all losses"
     (let [imminent-loss-state
           (new-test-state
-            [{:round 0 :player 0 :move-history :0} [
-              [{:round 0 :player 1 :move-history :00} []]]])
+            [{:round 0 :player 0 :path-to-state [0]} [
+              [{:round 0 :player 1 :path-to-state [0 0]} []]]])
           terminal-scorer
           (new-terminal-scorer {
-            {:round 0 :player 1 :move-history :00} 1
+            {:round 0 :player 1 :path-to-state [0 0]} 1
           })]
       (should= -1 (score-for-current-player
                     imminent-loss-state
                     terminal-scorer))))
 
-
   (it "looks one move ahead and sees a mixture of wins ties and losses"
     (let [win-soon-state
           (new-test-state
-            [{:round 0 :player 0 :move-history :0} [
-              [{:round 0 :player 1 :move-history :00} []]
-              [{:round 0 :player 1 :move-history :01} []]
-              [{:round 0 :player 1 :move-history :02} []]]])
+            [{:round 0 :player 0 :path-to-state [0]} [
+              [{:round 0 :player 1 :path-to-state [0 0]} []]
+              [{:round 0 :player 1 :path-to-state [0 1]} []]
+              [{:round 0 :player 1 :path-to-state [0 2]} []]]])
           terminal-scorer
           (new-terminal-scorer {
-            {:round 0 :player 1 :move-history :00} 1
-            {:round 0 :player 1 :move-history :01} 0
-            {:round 0 :player 1 :move-history :02} -1
+            {:round 0 :player 1 :path-to-state [0 0]} 1
+            {:round 0 :player 1 :path-to-state [0 1]} 0
+            {:round 0 :player 1 :path-to-state [0 2]} -1
           })]
       (should= -1 (score-for-current-player
                     win-soon-state
@@ -58,23 +62,23 @@
   (it "looks far into future and gets tie"
     (let [distant-tie-state
           (new-test-state
-            [{:round 0 :player 0 :move-history :0} [
-              [{:round 0 :player 1 :move-history :00} [
-                [{:round 1 :player 0 :move-history :000} []]
-                [{:round 1 :player 0 :move-history :001} []]
-                [{:round 1 :player 0 :move-history :002} []]]]
-              [{:round 0 :player 1 :move-history :01} [
-                [{:round 1 :player 0 :move-history :010} []]
-                [{:round 1 :player 0 :move-history :011} []]
-                [{:round 1 :player 0 :move-history :012} []]]]]])
+            [{:round 0 :player 0 :path-to-state [0]} [
+              [{:round 0 :player 1 :path-to-state [0 0]} [
+                [{:round 1 :player 0 :path-to-state [0 0 0]} []]
+                [{:round 1 :player 0 :path-to-state [0 0 1]} []]
+                [{:round 1 :player 0 :path-to-state [0 0 2]} []]]]
+              [{:round 0 :player 1 :path-to-state [0 1]} [
+                [{:round 1 :player 0 :path-to-state [0 1 0]} []]
+                [{:round 1 :player 0 :path-to-state [0 1 1]} []]
+                [{:round 1 :player 0 :path-to-state [0 1 2]} []]]]]])
           terminal-scorer
           (new-terminal-scorer {
-            {:round 1 :player 0 :move-history :000} 1
-            {:round 1 :player 0 :move-history :001} -1
-            {:round 1 :player 0 :move-history :002} 0
-            {:round 1 :player 0 :move-history :010} 0
-            {:round 1 :player 0 :move-history :011} 0
-            {:round 1 :player 0 :move-history :012} 0 
+            {:round 1 :player 0 :path-to-state [0 0 0]} 1
+            {:round 1 :player 0 :path-to-state [0 0 1]} -1
+            {:round 1 :player 0 :path-to-state [0 0 2]} 0
+            {:round 1 :player 0 :path-to-state [0 1 0]} 0
+            {:round 1 :player 0 :path-to-state [0 1 1]} 0
+            {:round 1 :player 0 :path-to-state [0 1 2]} 0 
           })]
       (should= 0 (score-for-current-player
                    distant-tie-state
@@ -83,23 +87,23 @@
   (it "looks far into future and gets loss"
     (let [distant-loss-state
           (new-test-state
-            [{:round 0 :player 0 :move-history :0} [
-              [{:round 0 :player 1 :move-history :00} [
-                [{:round 1 :player 0 :move-history :000} []]
-                [{:round 1 :player 0 :move-history :001} []]
-                [{:round 1 :player 0 :move-history :002} []]]]
-              [{:round 0 :player 1 :move-history :01} [
-                [{:round 1 :player 0 :move-history :010} []]
-                [{:round 1 :player 0 :move-history :011} []]
-                [{:round 1 :player 0 :move-history :012} []]]]]])
+            [{:round 0 :player 0 :path-to-state [0]} [
+              [{:round 0 :player 1 :path-to-state [0 0]} [
+                [{:round 1 :player 0 :path-to-state [0 0 0]} []]
+                [{:round 1 :player 0 :path-to-state [0 0 1]} []]
+                [{:round 1 :player 0 :path-to-state [0 0 2]} []]]]
+              [{:round 0 :player 1 :path-to-state [0 1]} [
+                [{:round 1 :player 0 :path-to-state [0 1 0]} []]
+                [{:round 1 :player 0 :path-to-state [0 1 1]} []]
+                [{:round 1 :player 0 :path-to-state [0 1 2]} []]]]]])
           terminal-scorer
           (new-terminal-scorer {
-            {:round 1 :player 0 :move-history :000} -1
-            {:round 1 :player 0 :move-history :001} -1
-            {:round 1 :player 0 :move-history :002} -1
-            {:round 1 :player 0 :move-history :010} 0
-            {:round 1 :player 0 :move-history :011} 0
-            {:round 1 :player 0 :move-history :012} 0
+            {:round 1 :player 0 :path-to-state [0 0 0]} -1
+            {:round 1 :player 0 :path-to-state [0 0 1]} -1
+            {:round 1 :player 0 :path-to-state [0 0 2]} -1
+            {:round 1 :player 0 :path-to-state [0 1 0]} 0
+            {:round 1 :player 0 :path-to-state [0 1 1]} 0
+            {:round 1 :player 0 :path-to-state [0 1 2]} 0
           })]
       (should= -1 (score-for-current-player
                     distant-loss-state
@@ -108,23 +112,23 @@
   (it "looks far into future and gets win"
     (let [distant-win-state
           (new-test-state
-            [{:round 0 :player 0 :move-history :0} [
-              [{:round 0 :player 1 :move-history :00} [
-                [{:round 1 :player 0 :move-history :000} []]
-                [{:round 1 :player 0 :move-history :001} []]
-                [{:round 1 :player 0 :move-history :002} []]]]
-              [{:round 0 :player 1 :move-history :01} [
-                [{:round 1 :player 0 :move-history :010} []]
-                [{:round 1 :player 0 :move-history :011} []]
-                [{:round 1 :player 0 :move-history :012} []]]]]])
+            [{:round 0 :player 0 :path-to-state [0]} [
+              [{:round 0 :player 1 :path-to-state [0 0]} [
+                [{:round 1 :player 0 :path-to-state [0 0 0]} []]
+                [{:round 1 :player 0 :path-to-state [0 0 1]} []]
+                [{:round 1 :player 0 :path-to-state [0 0 2]} []]]]
+              [{:round 0 :player 1 :path-to-state [0 1]} [
+                [{:round 1 :player 0 :path-to-state [0 1 0]} []]
+                [{:round 1 :player 0 :path-to-state [0 1 1]} []]
+                [{:round 1 :player 0 :path-to-state [0 1 2]} []]]]]])
           terminal-scorer
           (new-terminal-scorer {
-            {:round 1 :player 0 :move-history :000} 1
-            {:round 1 :player 0 :move-history :001} -1
-            {:round 1 :player 0 :move-history :002} -1
-            {:round 1 :player 0 :move-history :010} 1
-            {:round 1 :player 0 :move-history :011} -1
-            {:round 1 :player 0 :move-history :012} -1
+            {:round 1 :player 0 :path-to-state [0 0 0]} 1
+            {:round 1 :player 0 :path-to-state [0 0 1]} -1
+            {:round 1 :player 0 :path-to-state [0 0 2]} -1
+            {:round 1 :player 0 :path-to-state [0 1 0]} 1
+            {:round 1 :player 0 :path-to-state [0 1 1]} -1
+            {:round 1 :player 0 :path-to-state [0 1 2]} -1
           })]
       (should= 1 (score-for-current-player
                    distant-win-state
